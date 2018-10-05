a803530488cb895b8a5dafc56ddf17ef79f4f52b
Nicholas Jhirad <Nicholas.Jhirad@pinnaclesports.com>
2018-10-05 16:44:50 -0400

    added definitions for performance metric

---
+++ R/performance_metric.R
@@ -0,0 +1,14 @@
+weights_lookup =
+  list(
+    daily = 24/7,
+    weekly = 24/2,
+    hourly = 1
+  )
+
+norm_coefficient = function(true_mean_consumption, weight) {
+  weight / true_mean_consumption_i 
+}
+
+NMAE = function(predicted, actual, true_mean_consumption, pred_type) {
+  sum(abs(predicted - actual)) * norm_coefficient(true_mean_consumption, weights_lookup[pred_type])
+}
